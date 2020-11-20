e09da76138fa5d95b1b1356a68f89430ac499047
vijay-singh07 <vs8262836@gmail.com>
2020-11-20 19:29:00 +0530

    adding validation to home/dashboard

---
+++ home.php
@@ -0,0 +1,15 @@
+<?php 
+require_once("profile.php"); 
+?>
+
+<!DOCTYPE html>
+<html>
+	<head></head>
+		<body>	
+			<center>YOU ARE LOGGED IN</center>
+			<div id="profile">
+				<b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b>
+			</div>
+		</body>
+</html>
+
